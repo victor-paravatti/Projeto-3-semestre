85a76383e400c05ee576c774a7a7e321722ca3e6
victor-paravatti <59876332+victor-paravatti@users.noreply.github.com>
2021-01-09 16:12:15 -0300

    Table Create

--- core/models.py
+++ core/models.py
@@ -1,3 +1,43 @@
+from typing import Tuple
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    cpf = models.CharField(max_length=12)
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=100)
+    endereco= models.CharField(max_length=50)
+    cidade = models.CharField(max_length=100)
+    bairro = models.CharField(max_length=150)
+    cep = models.CharField(max_length=10)
+    email = models.CharField(max_length=100)
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome + '(' + str(self.id) + ')'
+
+    class Meta:
+        verbose_name_plural = 'Clientes'
+
+
+class Pedido(models.Model):
+    numero = models.CharField(max_length=10)
+    datentrega = models.CharField(max_length=8)
+    datpedido = models.CharField(max_length=8, blank=True, null=True)
+    valor
+
+
+    def __str__(self):
+        return self.nome + '(' + str(self.id) + ')'
+
+    class Meta:
+        verbose_name_plural = 'Pedidos'
+
+
+
+
+    
+
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,13 +1,25 @@
+from django.http import request
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'core/index.html')
 
 
+@login_required
 def carrinho(request):
-    return render(request, 'core/carrinho.html')
+    data = cartData(request)
+    carrinhoItems = data['carrinhoItems']
+    compra = data['compra']
+    items = data['items']
+    contexto = {'items':items, 'compra':compra, 'carrinhoItems':carrinhoItems}
+
+    return render(request, "core/carrinho.html", contexto)
 
 
+@login_required
 def login(request):
     return render(request, 'core/login.html')
+
+
     
