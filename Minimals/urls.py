70fc27b99d2fa7c2ebd0f59721fb1d9629c2f1e1
Demetrios <pantaleao15@hotmail.com>
2021-01-13 12:41:24 -0300

    creation of status's, fabricante's, pagamento's and tipoSolicitacao's views and templates

--- Minimals/urls.py
+++ Minimals/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from core.views import Cadastrar, home, carinho, cadastrar_cliente, cadastrar_produto, \
     listar_produto, editar_produto, excluir_produto, exibir_produto, cadastrar_funcionario, \
     listar_funcionario, editar_funcionario, excluir_funcionario, cadastrar_cargo, listar_cargo, \
-    editar_cargo, excluir_cargo, cadastrar_setor, listar_setor, editar_setor, excluir_setor
+    editar_cargo, excluir_cargo, cadastrar_setor, listar_setor, editar_setor, excluir_setor, \
+    cadastrar_fabricante, listar_fabricante, editar_fabricante, excluir_fabricante, cadastrar_status, \
+    listar_status, editar_status, excluir_status, cadastrar_tipo_solicitacao, listar_tipo_solicitacao, \
+    editar_tipo_solicitacao, excluir_tipo_solicitacao, cadastrar_pagamento, listar_pagamento, \
+    editar_pagamento, excluir_produto
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -31,7 +35,24 @@ urlpatterns = [
     path('cadastrar_setor/', cadastrar_setor, name='url_cadastrar_setor'),
     path('listar_setor/', listar_setor, name='url_listar_setor'),
     path('editar_setor/<int:id>/', editar_setor, name='url_editar_setor'),
-    path('excluir_setor/<int:id>/', excluir_setor, name='url_excluir_setor')
+    path('excluir_setor/<int:id>/', excluir_setor, name='url_excluir_setor'),
+    path('cadastrar_fabricante/', cadastrar_fabricante, name='url_cadastrar_fabricante'),
+    path('listar_fabricante/', listar_fabricante, name='url_listar_fabricante'),
+    path('editar_fabricante/<int:id>/', editar_fabricante, name='url_editar_fabricante'),
+    path('excluir_fabricante/<int:id>/', excluir_fabricante, name='url_excluir_fabricante'),
+    path('cadastrar_status/', cadastrar_status, name='url_cadastrar_status'),
+    path('listar_status/', listar_status, name='url_listar_status'),
+    path('editar_status/<int:id>/', editar_status, name='url_editar_status'),
+    path('excluir_status/<int:id>/', excluir_status, name='url_excluir_status'),
+    path('cadastrar_tipo_solicitacao/', cadastrar_tipo_solicitacao, name='url_cadastrar_tipo_solicitacao'),
+    path('listar_tipo_solicitacao/', listar_tipo_solicitacao, name='url_listar_tipo_solicitacao'),
+    path('editar_tipo_solicitacao/<int:id>/', editar_tipo_solicitacao, name='url_editar_tipo_solicitacao'),
+    path('excluir_tipo_solicitacao/<int:id>/', excluir_tipo_solicitacao, name='url_excluir_tipo_solicitacao'),
+    path('cadastrar_pagamento/', cadastrar_pagamento, name='url_cadastrar_pagamento'),
+    path('listar_pagamento/', listar_pagamento, name='url_listar_pagamento'),
+    path('editar_pagamento/<int:id>/', editar_pagamento, name='url_editar_pagamento'),
+    path('excluir_pagamento/<int:id>/', excluir_pagamento, name='url_excluir_pagamento')
+    
 
 ]
 
